d131bfe6da36068805c7ab4471a3c1e967956070
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-01-21 17:19:30 +0000

    First commit

---
+++ bloc_access/lib/cubit/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 0));
+
+  void increment() => emit(CounterState(counter: state.counter! + 1, wasIncremented: true));
+
+   void decrement() => emit(CounterState(counter: state.counter! - 1, wasIncremented: false));
+}
---
+++ bloc_access/lib/cubit/counter_state.dart
@@ -0,0 +1,10 @@
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int? counter;
+  bool? wasIncremented;
+  CounterState({
+    required this.counter,
+    this.wasIncremented
+  });
+}
