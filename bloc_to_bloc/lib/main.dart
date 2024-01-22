75ac06261a6c3daf3e4bd2c6c17ab1851e098ffb
Jakub-Lukac <jakub.lukac81@gmail.com>
2024-01-22 22:14:25 +0000

    First Commit

---
+++ bloc_to_bloc/lib/main.dart
@@ -0,0 +1,78 @@
+// ignore_for_file: unused_field, unused_element
+
+import 'package:bloc_to_bloc/logic/cubit/counter_cubit.dart';
+import 'package:bloc_to_bloc/logic/cubit/internet_cubit.dart';
+import 'package:bloc_to_bloc/presentation/router/app_router.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(
+    MyApp(
+      appRouter: AppRouter(),
+      connectivity: Connectivity(),
+    ),
+  );
+}
+
+// BlocProvider closes provided bloc/cubit automatically
+// BlocProvider.value - we have to manually close the provided bloc/cubit
+// we can only dispose it inside a StateFul widget!!
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, required this.appRouter, required this.connectivity});
+
+  //final CounterCubit _counterCubit = CounterCubit();
+  final AppRouter appRouter;
+  final Connectivity connectivity;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => InternetCubit(connectivity: connectivity),
+        ),
+        BlocProvider(
+          create: (context) => CounterCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        // passing function as pointer/argument
+        // we are NOT passing the return value of function!!!
+        onGenerateRoute: appRouter.onGenerateRoute,
+        /*routes: {
+            "/": (context) => BlocProvider.value(
+                  // no (context) => _counterCubit, because it would create 3 unique instances
+                  // we only want 1 unique instance of counter cubit
+                  value: _counterCubit,
+                  child: const HomeScreen(
+                    title: "HomeScreen",
+                    color: Colors.blueAccent,
+                  ),
+                ),
+            "/second": (context) => BlocProvider.value(
+                  value: _counterCubit,
+                  child: const SecondScreen(
+                    title: "Second Screen",
+                    color: Colors.redAccent,
+                  ),
+                ),
+            "/third": (context) => BlocProvider.value(
+                  value: _counterCubit,
+                  child: const ThirdScreen(
+                    title: "Third Screen",
+                    color: Colors.greenAccent,
+                  ),
+                ),
+          },*/
+      ),
+    );
+  }
+}
---
+++ bloc_to_bloc/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,162 @@
+import 'package:bloc_to_bloc/constants/enums.dart';
+import 'package:bloc_to_bloc/logic/cubit/internet_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../logic/cubit/counter_cubit.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key, this.title, this.color});
+
+  final String? title;
+  final Color? color;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<InternetCubit, InternetState>(
+      listener: (context, state) {
+        if (state is InternetConnected &&
+            state.connectionType == ConnectionType.wifi) {
+          context.read<CounterCubit>().increment();
+        } else if (state is InternetConnected &&
+            state.connectionType == ConnectionType.mobile) {
+          context.read<CounterCubit>().decrement();
+        } else if (state is InternetDisconnected) {
+        } else {}
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title!),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              BlocBuilder<InternetCubit, InternetState>(
+                builder: (context, state) {
+                  if (state is InternetConnected &&
+                      state.connectionType == ConnectionType.wifi) {
+                    return const Text("Wifi");
+                  } else if (state is InternetConnected &&
+                      state.connectionType == ConnectionType.mobile) {
+                    return const Text("Mobile");
+                  } else if (state is InternetDisconnected) {
+                    return const Text("Disconnected");
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              ),
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if (state.wasIncremented!) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Incremented!"),
+                        duration: Duration(
+                          milliseconds: 300,
+                        ),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Decrement!"),
+                        duration: Duration(
+                          milliseconds: 300,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                builder: (context, state) {
+                  if (state.counter! < 0) {
+                    return Text(
+                      "BRR NEGATIVE ${state.counter.toString()}",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  } else if (state.counter! % 2 == 0) {
+                    return Text(
+                      "YAAAY ${state.counter.toString()}",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  } else if (state.counter! == 5) {
+                    return Text(
+                      "HMM, NUMBER ${state.counter.toString()}",
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  } else {
+                    return Text(
+                      state.counter.toString(),
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    );
+                  }
+                },
+              ),
+              /*Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () => context.read<CounterCubit>().decrement(),
+                      tooltip: 'Decrement',
+                      child: const Icon(Icons.remove),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () => context.read<CounterCubit>().increment(),
+                      tooltip: 'Increment',
+                      child: const Icon(Icons.add),
+                    ),
+                  ],
+                ),*/
+              const SizedBox(height: 24),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: widget.color,
+                ),
+                onPressed: () {
+                  /*Navigator.of(context).push(
+                      MaterialPageRoute(
+                        // (_) because it can not be named context
+                        // we want to refer to the context of Navigator.of(context)
+                        // and not a brand new context
+                        builder: (_) {
+                          return BlocProvider.value(
+                            value: BlocProvider.of<CounterCubit>(context),
+                            child: const SecondScreen(
+                              title: "Second Screen",
+                              color: Colors.red,
+                            ),
+                          );
+                        },
+                      ),
+                    );*/
+                  Navigator.of(context).pushNamed("/second");
+                },
+                child: const Text("Go to second screen"),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: widget.color,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pushNamed("/third");
+                },
+                child: const Text("Go to Third screen"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
